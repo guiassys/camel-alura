12d57da302f5dde7bc16ef6287e37ce2bd507a31
guiassys <guiassys@gmail.com>
2023-08-20 17:26:41 -0300

    Versiona exemplo de integração camel
    Copia arquivos de um diretório e guarda em outro.

---
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -0,0 +1,34 @@
+package br.com.caelum.camel;
+
+
+import java.util.logging.Logger;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class RotaPedidos {
+
+	private static final Logger logger = Logger.getLogger(RotaPedidos.class.getName());
+	public static void main(String[] args) throws Exception {
+
+		logger.info("Inicio do programa");
+
+		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(
+				new RouteBuilder() {
+
+					@Override
+					public void configure() throws Exception {
+						from("file:pedidos").
+								to("file:saida");
+					}
+				}
+		);
+
+		context.start();
+		Thread.sleep(20000);
+		context.stop();
+		logger.info("Fim do programa");
+	}
+}
